2126eb21e3dad82c4adae1cb2be835ef0d7c8a0d
muniquefeitoz4@gmail.com <muniquefeitoz4@gmail.com>
2023-11-04 12:36:46 -0300

    fazendo o react js funcionar

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather api</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
