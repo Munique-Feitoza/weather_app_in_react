381912ed7b258855ef16685e1d4752c95cfe9243
muniquefeitoz4@gmail.com <muniquefeitoz4@gmail.com>
2023-11-18 10:57:12 -0300

    transformando ReactJS em React

--- config.js
+++ config.js
@@ -1,14 +1,8 @@
 SystemJS.config({
   baseURL:'https://unpkg.com/',
-  defaultExtension: true,
-  packages: {
-    ".": {
-      main: './App.js',
-      defaultExtension: 'js'
-    }
-  },
+  defaultExtension: 'true',
   meta: {
-    '*.js': {
+    '*.jsx': {
       'babelOptions': {
         react: true
       }
@@ -17,11 +11,15 @@ SystemJS.config({
   map: {
     'plugin-babel': 'systemjs-plugin-babel@latest/plugin-babel.js',
     'systemjs-babel-build': 'systemjs-plugin-babel@latest/systemjs-babel-browser.js',
-    'react': 'react@18/umd/react.development.js',
-    'react-dom': 'react-dom@18/umd/react-dom.development.js'
+    'react': 'react@16.14.0/umd/react.development.js',
+    'react-dom': 'react-dom@16.14.0/umd/react-dom.development.js',
+    'prop-types': 'prop-types@15.6/prop-types.js',
+    'classnames': 'classnames@2.2.6/index.js',
+    '@material-ui/core': '@material-ui/core@3.2.2/umd/material-ui.development.js',
+    '@material-ui/icons': '@material-ui/icons@3.0.1/index.js',
   },
   transpiler: 'plugin-babel'
 });
 
-SystemJS.import('./App')
+SystemJS.import('./App.jsx')
   .catch(console.error.bind(console));
